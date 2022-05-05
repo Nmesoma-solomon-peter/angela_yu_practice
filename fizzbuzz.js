5a7e2cd0831d2794ec7654c8f02a2b5fe5250c3b
nmesoma <nmesomasolo2809@gmail.com>
2022-05-05 12:04:08 +0100

    fizzbuzz exercise1

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+// A program that prints the number from 1 to 100. But for Multiples of three Print "FIZZ"
+//  instead of the number and for the multiples of five print "BUZZ". 
+//  for numbers which are multiples of both three and five print "FizzBuzz".
+
+for(i = 1;i <=100; i++ ){
+    if(i % 3 === 0 && i % 5 === 0){  
+        console.log("FIZZBUZZ");
+    }else if(i % 3 === 0){
+        console.log("FIZZ");
+    }
+    else if( i % 5 === 0){
+        console.log("BUZZ");
+    }else{
+        console.log(i);
+    }
+}
+
